d6d03fabdf0c88eba598229ef50e3add7ea40769
Christian Bonilla <christianurielbonillasuarez@gmail.com>
2024-12-13 18:40:12 -0800

    Complete assignment 1

---
+++ js/assignment1.js
@@ -0,0 +1,62 @@
+console.log("--- Funny Story ---");
+
+// Story variables
+const characterName = "Captain Wobblepants"; // Name of the main character
+let action = "danced"; // Action performed by the character
+const object = "a giant rubber duck"; // Object involved in the story
+let place = "on top of the Eiffel Tower"; // Place where the story happens
+const twist = "turned into a talking pineapple"; // Unexpected twist
+
+// Generate the story
+let story = `Once upon a time, ${characterName} ${action} with ${object} ${place}. 
+But then, something unexpected happened! The rubber duck ${twist}.`;
+
+// Output the story to the console
+console.log(story);
+
+// Change variables to create a new version of the story
+let newCharacterName = "Dr. Giggles"; // New character name
+action = "sang opera"; // New action
+place = "in a haunted library"; // New place
+const newTwist = "started juggling flaming marshmallows"; // New twist
+
+// Generate the new story
+let newStory = `In another tale, ${newCharacterName} ${action} while holding ${object} ${place}. 
+Suddenly, the scene took a wild turn as the rubber duck ${newTwist}.`;
+
+// Output the new story to the console
+console.log(newStory);
+
+// let nameCharacter = "Luis";
+// let place = "school";
+// let time = "8";
+// let anotherTime = "6";
+
+// const familyMember = "sister";
+
+// console.log(`Hello, my nameCharacter is ${nameCharacter}. I am going to the ${place} at ${time} o'clock.`);
+// console.log("One day " + nameCharacter + " was late for " + place + ". It was " + time + " o'clock and " + nameCharacter + " was in a hurry.");
+// console.log("when the star time was supposed to be at " + anotherTime + " o'clock.");
+// console.log("My " + familyMember + " was there on time!");
+
+// console.log("------------------Another story------------------");
+
+// nameCharacter = "Giancarlo";
+// place = "work";
+// time = "12";
+// anotherTime = "8";
+
+// console.log("One day " + nameCharacter + " was late for " + place + ". It was " + time + " o'clock and " + nameCharacter + " was in a hurry.");
+// console.log("when the star time was supposed to be at " + anotherTime + " o'clock.");
+// console.log("My " + familyMember + " was there on time!");
+
+// console.log("------------------Another story with a function------------------");
+
+// Transform the code above into a function
+// function story(nameCharacter, place, time, anotherTime, familyMember) {
+//     console.log("One day " + nameCharacter + " was late for " + place + ". It was " + time + " o'clock and " + nameCharacter + " was in a hurry.");
+//     console.log("when the star time was supposed to be at " + anotherTime + " o'clock.");
+//     console.log("My " + familyMember + " was there on time!");
+// }
+
+// story("Luis", "school", "8", "6", "sister");
